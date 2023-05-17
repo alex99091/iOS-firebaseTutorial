074d150b3c8e8c32d5e7a82be8350c348de1c4f0
alex99091 <bkk99091@gmail.com>
2023-05-17 15:35:24 +0900

    FEAT: create basic setting for firebase Realtime DataBase

---
+++ firebaseTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebaseTutorial
+//
+//  Created by BOONG KI KWAK on 2023/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
